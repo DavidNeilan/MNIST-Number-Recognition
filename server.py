99ae3aa1e17488b4ffdc82772afe4147ff6a7c12
David Neilan <neilan.david@gmail.com>
2019-12-13 23:18:54 +0000

    Added significant documentation
    * Documented notebook better
    * Documented Flask app better

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 import base64
+import tempfile
 
 import cv2
 import numpy as np
@@ -13,11 +14,15 @@ model = tf.keras.models.load_model('mnist.model')
 def index():
     if request.method == 'POST':
 
-        with open('img.png', 'wb') as file:
+        temp_file = tempfile.NamedTemporaryFile()
+        with open(temp_file.name, 'wb') as file:
             file.write(base64.standard_b64decode(request.form.get('canvas-data', '')))
 
-        image = cv2.imread('img.png', cv2.IMREAD_UNCHANGED)
+        image = cv2.imread(temp_file.name, cv2.IMREAD_UNCHANGED)
 
+        # Adapted from: https://stackoverflow.com/a/46275857
+        # Crop the image to a bounding box containing only the non transparent pixels
+        # Get all nonzero alpha(Alpha is channel 3) coordinates
         y, x = image[:, :, 3].nonzero()
         min_x, min_y = np.min(x), np.min(y)
         max_x, max_y = np.max(x), np.max(y)
@@ -26,21 +31,27 @@ def index():
 
         transparency_mask = image[:, :, 3] == 0
 
+        # Replace areas of transparency with white and not transparent
         image[transparency_mask] = [255, 0, 0, 0]
 
         greyscale_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
         greyscale_mask = cv2.compare(greyscale_image, 5, cv2.CMP_LT)
 
+        # Turn every non-black(grey) pixel into white. Allows image to be drawn as black on transparent and converted to
+        # better match the actual MNIST model
         image[greyscale_mask > 0] = 255
         image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
         image = cv2.threshold(image, 127, 255, cv2.THRESH_BINARY)[1]
 
+        # Blurring the image makes the image easier to detect
         image = cv2.GaussianBlur(image, (0, 0), cv2.BORDER_DEFAULT)
         image = cv2.resize(image, (20, 20))
 
+        # Creates a black image of size 28x28
         processed_image = np.zeros((28, 28), np.uint8)
         processed_image[4:4 + image.shape[0], 4:4 + image.shape[1]] = image
 
+        # Reshape, and normalise before passing to model
         data = model.predict(processed_image.reshape(1, 784).astype('float32') / 255.0)[0]
 
         return render_template('index.html', result=np.argmax(data), data=dict(enumerate(data.tolist())))
